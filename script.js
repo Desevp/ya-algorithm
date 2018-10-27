ed31d427ddf77a741b4c4a44c78c17e9db4880a9
Jane Des <desevp@gmail.com>
2018-10-27 14:40:20 +0300

    Added bubble sort

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// Сортировка пузырьком
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([3, 0, 443, 4, 1, 6, 32]));
